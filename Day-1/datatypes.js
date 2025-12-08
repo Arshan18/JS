384a414c621c113844e0516742849b55f4f92cca
Arshan <arshanattar09@gmail.coom>
2025-12-08 23:30:28 +0530

    Fix Day-1 and re-add clean version

---
+++ Day-1/datatypes.js
@@ -0,0 +1,53 @@
+/*
+
+Primitive Datatypes
+
+String
+Number
+Boolean
+Big int
+
+Undefined
+Null
+Symbol
+
+Non-Primitive
+
+Object
+Array 
+Function
+
+*/
+
+// Numbers
+
+let num1 = 0xf;
+console.log(num1);
+
+let num = 100000;
+console.log(num);
+
+let num2 = -110 / 0;
+console.log(num2);
+
+console.log(Number.MIN_VALUE);
+
+number = 12343029898764512345678n;
+console.log(number);
+
+// String
+
+let user = 'Arshan \n"Attar"';
+console.log(user);
+
+let firstName = "Arshan";
+let lastName = "Attar";
+let Name = firstName + " " + lastName;
+
+console.log(Name);
+
+// Boolean
+
+let bool = 5 > 6;
+console.log(bool);
+console.log(typeof bool);
