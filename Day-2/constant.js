9d51c9b680713420146b7ac093d1b09f95bd9555
Arshan <arshanattar09@gmail.coom>
2025-12-08 23:31:35 +0530

    Add Day-2 files

---
+++ Day-2/constant.js
@@ -0,0 +1,10 @@
+let radius = 10;
+const pi = 3.14;
+let area;
+
+radius = 5;
+// pi = 2;
+
+// area = pi * radius square
+area = pi * radius * radius;
+console.log(area);
---
+++ Day-2/variable.js
@@ -0,0 +1,13 @@
+let gameName = "BGMI";
+gameName = "FF";
+console.log(gameName);
+
+let userName = "ArshanAttar";
+userName = "Arshan";
+console.log(userName);
+
+let num = 18;
+console.log(num);
+
+num = num + 45;
+console.log(num);
