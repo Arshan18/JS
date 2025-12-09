4d03bf682bc32dde931c2ce386497a3395344e42
Arshan <arshanattar09@gmail.coom>
2025-12-10 00:04:36 +0530

    Learn Operation

---
+++ Day-3/logical.js
@@ -0,0 +1,20 @@
+// && -> and
+// || -> or
+// ! -> reverse
+
+let isLoggedin = true;
+let isPaid = false;
+
+console.log(isLoggedin && isPaid);
+
+let isEmailuser = true;
+let isGoogleuser = false;
+
+console.log(isEmailuser || isGoogleuser);
+
+// Assignment operation
+
+let num = 730;
+num += 10;
+
+console.log(num);
