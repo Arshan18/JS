b942ae8845547f2007653fe56391ed6e384ff26c
Arshan <arshanattar09@gmail.coom>
2025-12-10 00:12:11 +0530

    Add JS file

---
+++ Day-3/loops.js
@@ -0,0 +1,17 @@
+/*
+1. Write a 'while' loop that calculates the sum of all numbers from 1 to 5 and stores the result in a variable name 'sum'.
+*/
+
+let sum = 0;
+let i = 1;
+
+while (i <= 5) {
+  sum = sum + i;
+  i++;
+}
+
+console.log(sum);
+
+/*
+Write a 'while' loop that counts down fromm 5 to 1 and stores the numbers in an array name 'countdown'.
+*/
