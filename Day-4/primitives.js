4acddd18029f0350ddd1804d0ec1dddeb2a3e12f
Arshan <arshanattar09@gmail.com>
2025-12-10 11:35:51 +0530

    Learning

---
+++ Day-4/primitives.js
@@ -0,0 +1,48 @@
+// Number
+
+let balance = 120;
+let amount = new Number(120);
+
+console.log(balance);
+console.log(amount);
+
+console.log(balance);
+console.log(typeof amount);
+
+// Boolean
+
+let isActive = true;
+let isOnline = new Boolean(true);
+
+console.log(isOnline);
+console.log(isActive);
+
+// Null & Undefined
+
+let firstName = null;
+let lastName = undefined;
+
+console.log(firstName);
+console.log(lastName);
+
+// String
+
+let Str = "Arshann";
+let Str1 = "Attar";
+let userName = "Virat";
+
+let oldGreet = Str + " Hello";
+console.log(oldGreet);
+
+let greetMessage = `Hello ${userName} !`;
+let sum = `Value is ${10 + 8}`;
+console.log(greetMessage);
+console.log(sum);
+
+// Symbol
+
+let sym1 = Symbol("Arshan");
+let sym2 = Symbol("Attar");
+
+console.log(sym1);
+console.log(sym2);
