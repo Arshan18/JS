631dc7477d41c0a9257bf5471349104dc719c24d
Arshan <arshanattar09@gmail.com>
2025-12-11 15:24:38 +0530

    Learning

---
+++ Day-4/Nonprimitives.js
@@ -0,0 +1,27 @@
+// Objects
+
+const userName = {
+  firstName: "Arshan",
+  Age: 20,
+  isLoggedin: true,
+};
+
+userName.firstName = "Mr. Ash";
+userName.lastName = "Attar";
+
+console.log(userName.firstName);
+console.log(userName.lastName);
+
+console.log(userName);
+
+let today = new Date();
+console.log(today);
+
+// Arrays
+
+let newuserName = ["Arshan", true];
+console.log(newuserName[0]);
+
+let isValue = true;
+console.log(Number(isValue));
+console.log(typeof Number(null));
