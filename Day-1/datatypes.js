181cce9888229075bbb83798a40b521d46011bf4
Arshan <arshanattar09@gmail.com>
2025-12-18 23:46:35 +0530

    learning

--- Day-1/datatypes.js
+++ Day-1/datatypes.js
@@ -51,3 +51,41 @@ console.log(Name);
 let bool = 5 > 6;
 console.log(bool);
 console.log(typeof bool);
+
+/*
+Write a function named `makeTea` that takes one parameter, `typeofTea`, and returns a string like `"Making green tea"` when called with `"green tea"`.
+Stores the result in a variable name `teaOrder`.
+*/
+
+function makeTea(typeOfTea) {
+  return `Making ${typeOfTea}`;
+}
+
+let teaOrder = makeTea("green tea");
+console.log(teaOrder);
+
+/*
+Write a 'while' loop that calculates the sum of all numbers from 1 to 5 and stores the result in a variable name 'sum'.
+*/
+
+let sum = 0;
+let i = 1;
+
+while (i <= 5) {
+  sum = sum += i;
+  i++;
+}
+console.log(sum);
+
+/*
+Write a 'while' loop that counts down fromm 5 to 1 and stores the numbers in an array name 'countdown'.
+*/
+
+let countdown = [];
+let j = 5;
+
+while (j >= 1) {
+  countdown.push(j);
+  j--;
+}
+console.log(countdown);
