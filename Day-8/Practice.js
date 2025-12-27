42e63f4a4ab0a1f389a70452f7c4dd36b6dfee7a
Arshan <arshanattar09@gmail.com>
2025-12-27 19:48:22 +0530

    learning

--- Day-8/Practice.js
+++ Day-8/Practice.js
@@ -172,3 +172,44 @@ function flattenArray(arr) {
 }
 
 console.log(flattenArray(["Arshan", [18, 20], ["Attar"], [2005]]));
+
+// Write a function sumOfN(n) that returns the sum of the first n natural numbers
+
+function sumOfN(n) {
+  let sum = 0;
+
+  for (let i = 0; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(sumOfN(12));
+
+// Write a function printMultiplicationTable(n) that returns the multiplication table for n. Values return in the array must be of the format 2 * 2 = 4
+
+function printMultiplicationTable(n) {
+  let table = [];
+
+  for (let i = 1; i <= 10; i++) {
+    table.push(`${n} * ${i} = ${n * i}`);
+  }
+  return table;
+}
+
+console.log(printMultiplicationTable(6));
+
+// Write a function countVowels(str) that returns the number of vowels (in both lower & uppercase) in the given string str.
+
+function countVowels(str) {
+  let count = 0;
+  let vowels = "aeiouAEIOU";
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countVowels("learning"));
