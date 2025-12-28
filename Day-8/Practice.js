1af9ad8b4c9bbcd5fe43fb9506fa571279a63833
Arshan <arshanattar09@gmail.com>
2025-12-28 21:46:45 +0530

    learning

--- Day-8/Practice.js
+++ Day-8/Practice.js
@@ -213,3 +213,33 @@ function countVowels(str) {
 }
 
 console.log(countVowels("learning"));
+
+// Write a function squareNumbers(arr) using map() and arrow functions
+
+const squareNumbers = (arr) => arr.map((num) => num * num);
+console.log(squareNumbers([1, 2, 3, 4]));
+
+// Create a function filterEvenNumbers(arr) using filter() and arrow functions
+
+const filterEvenNumbers = (arr) => arr.filter((num) => num % 2 === 0);
+console.log(filterEvenNumbers([1, 2, 3, 23, 13, 14]));
+
+// Write a function sumPositiveNumbers(arr) that takes an array of numbers and returns the sum of all positive numbers using filter() and reduce() with arrow functions
+
+const sumPositiveNumbers = (arr) =>
+  arr.filter((num) => num > 0).reduce((sum, num) => sum + num, 0);
+console.log(sumPositiveNumbers([-1, 2, 3, -4, 5]));
+
+// Write a function getNames(arr) that takes an array of objects where each object has a name property, and returns an array of just the names using map() and arrow functions
+
+const getNames = (arr) => arr.map((item) => item.name);
+console.log(getNames([{ name: "Arshan" }, { name: "Attar" }]));
+
+// Write a function findLongestWord(arr) that takes an array of strings and returns the longest word using reduce() and an arrow function
+
+const findLongestWord = (arr) =>
+  arr.reduce(
+    (longest, current) => (current.length > longest.length ? current : longest),
+    ""
+  );
+console.log(findLongestWord(["javascript", "python", "java"]));
