d2e7349b0fd2db2e4e3bd67b8c91dc1361ed7773
Arshan <arshanattar09@gmail.com>
2025-12-30 19:56:17 +0530

    learning

---
+++ Day-9/constructorFunction.js
@@ -0,0 +1,51 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+let myInfo = new Person("Arshan", 20);
+console.log(myInfo);
+
+function Car(company, model, launchDate) {
+  this.company = company;
+  this.model = model;
+  this.launchDate = launchDate;
+}
+
+let myCar = new Car("BMW", "M4", "May 2024");
+console.log(myCar);
+
+let myNewCar = new Car("Mahindra", "TharRoxx", "Aug 2024");
+console.log(myNewCar);
+
+function Gun(type) {
+  this.type = type;
+  this.describe = function () {
+    return `This is the most loved gun in bgmi ${this.type}`;
+  };
+}
+
+let lovedGun = new Gun("M416");
+console.log(lovedGun.describe());
+
+function Bird(species) {
+  this.species = species;
+}
+
+Bird.prototype.sound = function () {
+  return `${this.species} makes a sound`;
+};
+
+let myBird = new Bird("Parrot");
+console.log(myBird.sound());
+
+function Drink(name) {
+  if (!new.target) {
+    throw new Error("Drink must be called with the new keyword");
+  }
+  this.name = name;
+}
+
+let tea = new Drink("Tea");
+
+let coffee = Drink("coffee");
