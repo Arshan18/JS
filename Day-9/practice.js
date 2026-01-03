253391a558e6c30a3eb6a5f25e72638d267b42a4
Arshan <arshanattar09@gmail.com>
2026-01-03 23:21:32 +0530

    Solving OOPs Questions

--- Day-9/practice.js
+++ Day-9/practice.js
@@ -24,6 +24,22 @@ class Dog extends Animal {
 }
 
 let myDog = new Dog("Mammal");
-console.log(myDog.type);
-console.log(myDog.speak());
-console.log(myDog.bark());
+// console.log(myDog.type);
+// console.log(myDog.speak());
+// console.log(myDog.bark());
+
+/*
+2.Create a functional constructor Person that takes name and age as parameters. Add a method greet() to the constructor that returns "Hello, my name is [name] and i am [age] years old.".
+*/
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+
+  this.greet = function () {
+    return `Hello, my name is ${this.name} and i am ${this.age} years old.`;
+  };
+}
+
+let person1 = new Person("Arshan", 20);
+console.log(person1.greet());
