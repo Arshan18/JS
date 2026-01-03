67c55a212cc4bfe4b9149ac1561dca95a8e9d465
Arshan <arshanattar09@gmail.com>
2026-01-03 23:52:11 +0530

    Solving OOPs Questions

--- Day-9/practice.js
+++ Day-9/practice.js
@@ -50,9 +50,37 @@ function Person(name, age) {
   };
 }
 
-let person1 = new Person("Arshan", -20);
-console.log(person1.greet());
+let person1 = new Person("Arshan", 20);
+// console.log(person1.greet());
 
 /*
-Modify the Person constructor to throw an error if the age is not a positive number.
+3.
+Task-1.
+Create a class Vehicle with properties make and model, and a method getDetails() that returns a string "Make: [make], Model: [model]". Create a subclass Car that extends Vehicle and adds a method startEngine() that returns "Engine started".
+
+Task-2
+Extend the Vehicle class from the previous task to include a method move() that returns "The vehicle is moving". Then, override the move() method in the Car class to return "The car is driving".
+
+Task-3
+Add a static method isVehicle(obj) to the Vehicle class that checks if a given object is an instance of Vehicle. The method should return true if the object is a Vehicle or a subclass of Vehicle, and false otherwise.
 */
+
+class Vehicle {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+
+  getDetails() {
+    return `Make: ${this.make}, Model: ${this.model}`;
+  }
+}
+
+class Car extends Vehicle {
+  startEngine() {
+    return `Engine started`;
+  }
+}
+
+let myCar = new Car("BMW", "M4");
+console.log(myCar);
