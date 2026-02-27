ab62f32ec4a669eba929471b1a626e171b84085d
Arshan <arshanattar09@gmail.com>
2026-02-28 01:13:30 +0530

    Learning Advanced JS

--- Day-11/explore1.js
+++ Day-11/explore1.js
@@ -1,5 +1,13 @@
 console.log("Learning JS");
 
+function sayHello() {
+  console.log(" I would like to say Hello");
+}
+
+setTimeout(() => {
+  sayHello();
+}, 5s000);
+
 for (let index = 1; index <= 10; index++) {
   console.log(index);
 }
