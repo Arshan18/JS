8f10d1216f858bc6efea829cc62cc6bb0f0e6d6e
Arshan <arshanattar09@gmail.com>
2026-03-03 22:58:04 +0530

    Learning Advanced JS

---
+++ Day-11/appC.js
@@ -0,0 +1,5 @@
+const mathOperations = require("./mathOperationsC.js");
+
+console.log(mathOperations.add(2, 2));
+console.log(mathOperations.subtract(3, 2));
+console.log(mathOperations.multiply(2, 2));
