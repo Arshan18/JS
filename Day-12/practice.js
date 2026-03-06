a1c444d1ba231fd5522a5f6997fd618e6ef0bc2a
Arshan <arshanattar09@gmail.com>
2026-03-06 20:36:49 +0530

    Learning Advanced JS

--- Day-12/practice.js
+++ Day-12/practice.js
@@ -8,4 +8,24 @@ function simulateAsyncTask() {
   }, 2000);
 }
 
-simulateAsyncTask();
+// simulateAsyncTask();
+
+// Create a function simulateMultipleTasks() that starts three asynchronous tasks with different delays (1 second, 2 seconds, and 3 seconds).Each task should log "Task [n] finished" where [n] is the task number. Ensure the tasks run asynchronously.
+
+function simulateMultipleTasks() {
+  console.log("Simulating multiple task");
+
+  setTimeout(() => {
+    console.log("Task 1 finished");
+  }, 1000);
+
+  setTimeout(() => {
+    console.log("Task 2 finished");
+  }, 2000);
+
+  setTimeout(() => {
+    console.log("Task 3 finished");
+  }, 3000);
+}
+
+simulateMultipleTasks();
