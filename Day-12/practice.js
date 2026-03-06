70bbc304166cb940bab4595f3bd1e99c8723363d
Arshan <arshanattar09@gmail.com>
2026-03-07 01:22:35 +0530

    Learning Advanced JS

--- Day-12/practice.js
+++ Day-12/practice.js
@@ -54,5 +54,33 @@ function createCounter() {
 }
 
 let increment = createCounter();
-console.log(increment());
-console.log(increment());
+// console.log(increment());
+// console.log(increment());
+
+// Create a function rateLimiter(fn, limit) that returns a new function. The returned function allows calling fn only once within a limit time in milliseconds. If it is called again before the limit is reached, it should return "Rate limit exceeded".
+
+function rateLimiter(fn, limit) {
+  let lastCalled = 0;
+  return function (...args) {
+    const now = Date.now();
+    if (now - lastCalled < limit) {
+      return "Rate limit exceeded";
+    } else {
+      lastCalled = now;
+      return fn(...args);
+    }
+  };
+}
+
+function greet(name) {
+  return `Hello ${name}`;
+}
+
+const limitedGreet = rateLimiter(greet, 3000);
+
+console.log(limitedGreet("Arshan"));
+console.log(limitedGreet("Arshan"));
+
+setTimeout(() => {
+  console.log(limitedGreet("Arshan"));
+}, 3100);
